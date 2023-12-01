cb734c57b4dc9f0b91f12ee92999259ad4028b43
JensenMPillay <jensen_pillay@hotmail.fr>
2023-12-01 11:52:21 +0100

    Fix: VERCEL_URL Rewrited

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,6 +8,6 @@ export function cn(...inputs: ClassValue[]) {
 export function absoluteUrl(path: string) {
   // Verify Client Side
   if (typeof window !== "undefined") return path;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
+  if (process.env.VERCEL_URL) return `https://pdfai-seven.vercel.app${path}`;
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
