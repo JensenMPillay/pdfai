639ae764368856686f9b365d94bf016e0193d0c1
JensenMPillay <jensen_pillay@hotmail.fr>
2023-12-06 22:55:52 +0100

    Fix: Syntax

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
-import { string } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
